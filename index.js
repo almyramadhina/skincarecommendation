6e6b6e8717216795c348c3d6d0d2531fa8cd7b36
calvin <calvinirianto@gmail.com>
2020-11-29 23:03:40 +0700

    fix pooling database

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ require('dotenv').config() //buat env
 
 app.use(serveStatic(path.join(__dirname,"/FE"))) //serve static files in FE Folder
 
-const pool = new Pool() //create DB connection from env
-
 /*
   parameterized query
   const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
@@ -18,6 +16,8 @@ const pool = new Pool() //create DB connection from env
   pool.query(text, values)
 */
 app.get('/test', async (req, result) => {
+  const pool = new Pool() //create DB connection from env
+  
   pool.query('SELECT * FROM skintype', (err, res) => {
     if (err){
       throw err
