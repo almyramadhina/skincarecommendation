6723d0589a9612d6dc3a5ad2cc2461be9cc3a44b
Almyra Ramadhina <45095709+almyramadhina@users.noreply.github.com>
2020-12-03 02:55:54 +0700

    tambah cookie

--- FE/questions.js
+++ FE/questions.js
@@ -1,38 +1,43 @@
-               
 function sendForm() {
     var umur = $('input[name="age"]:checked').val();
     var skintype = $('input[name="type"]:checked').val();
     var skinprob = $('input[name="prob"]:checked').val();
     var kategori = $('input[name="prod"]:checked').val();
+    var hargabawah = $('input[name="prc1"]').val();
+    var hargaatas = $('input[name="prc2"]').val();
     var alergi = $("#allg input:checkbox:checked").map(function () {
                  return $(this).val();
                  }).get();
     var alergi1 = alergi[0];
     var alergi2 = alergi[1];
- 
+    // var dataStore = $.cookie("basket-data", JSON.stringify($("#InputForm").data()));
+                 
     var data = '';
     if (alergi.length == 1 && alergi[0] == 'none' ) {
-        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": 0, "hargaatas":100000, "kategori": kategori};
+        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas":hargaatas, "kategori": kategori};
     } else if (alergi.length == 1 &&  alergi[0] != 'none' ) {
-        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": 0, "hargaatas":100000, "kategori": kategori, alergi1:alergi[0]};
+        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas":hargaatas, "kategori": kategori, alergi1:alergi[0]};
     } else {
-        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": 0, "hargaatas":100000, "kategori": kategori, alergi1:alergi[0], alergi2:alergi[1]};
+        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas":hargaatas, "kategori": kategori, alergi1:alergi[0], alergi2:alergi[1]};
     }
 
+    // add cookie
+    var datastore = JSON.stringify(data);
+    document.cookie = "data=" + datastore;
+
     console.log(data);
-    
+
     fetch("/recommend", {
-            method: "POST",
-            headers: {
-                "Accept":"application/json",
-                "Content-Type":"application/json; charset=utf-8",
-            },
-            data: JSON.stringify(data)
-        }).then(async (response) => {
-            const content = await response.json();
-            console.log(content);
-        }).catch((error) => {
-            console.error(error);
+        method: "POST",
+        headers: {
+            "Accept":"application/json",
+            "Content-Type":"application/json; charset=utf-8",
         },
-    }
-    
+        body: JSON.stringify(data)
+    }).then(async (response) => {
+        const content = await response.json();
+        console.log(content);
+    }).catch((error) => {
+        console.error(error);
+    })
+}
