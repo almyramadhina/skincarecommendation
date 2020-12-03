178ee5552ace53872491ba29a45542e66cd20d95
dwidnw <55821468+dwidnw@users.noreply.github.com>
2020-12-03 10:37:50 +0700

    done

--- FE/questions.js
+++ FE/questions.js
@@ -2,24 +2,27 @@
 function sendForm() {
     
     var umur = $('input[name="age"]:checked').val();
+    var isTrue17 = umur == 'true';
     var skintype = $('input[name="type"]:checked').val();
     var skinprob = $('input[name="prob"]:checked').val();
     var kategori = $('input[name="prod"]:checked').val();
-    var hargabawah = $('input[name="prc1"]').val();
-    var hargaatas = $('input[name="prc2"]').val();
+    var hargabawah = parseInt(document.getElementById("prc1").value);
+    var hargaatas = parseInt(document.getElementById("prc2").value);
     var alergi = $("#allg input:checkbox:checked").map(function () {
                  return $(this).val();
                  }).get();
     var alergi1 = alergi[0];
     var alergi2 = alergi[1];
- 
+    
+    console.log(hargaatas);
+
     var data = '';
     if (alergi.length == 1 && alergi[0] == 'none' ) {
-        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas": hargaatas, "kategori": kategori};
+        data = { "umur": isTrue17, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas": hargaatas, "kategori": kategori};
     } else if (alergi.length == 1 &&  alergi[0] != 'none' ) {
-        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas": hargaatas, "kategori": kategori, alergi1:alergi[0]};
+        data = { "umur": isTrue17, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas": hargaatas, "kategori": kategori, alergi1:alergi[0]};
     } else {
-        data = { "umur": umur, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas": hargaatas, "kategori": kategori, alergi1:alergi[0], alergi2:alergi[1]};
+        data = { "umur": isTrue17, "skintype": skintype, "skinprob": skinprob, "hargabawah": hargabawah, "hargaatas": hargaatas, "kategori": kategori, alergi1:alergi[0], alergi2:alergi[1]};
     }
 
     console.log(data);
